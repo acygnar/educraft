b2a4a23450131d5a3df0e123dc6b0e943f5ef290
Adrian Cygnar <adrian.cygnar@gmail.com>
2026-04-19 22:03:43 +0200

    add nip and validation

--- functions.php
+++ functions.php
@@ -14,3 +14,4 @@ require_once get_template_directory() . '/incl/sitesetup.php';
 require_once get_template_directory() . '/incl/cpt.php';
 require_once get_template_directory() . '/incl/enqueue.php';
 require_once get_template_directory() . '/incl/ajax.php';
+require_once get_template_directory() . '/incl/nip.php';
---
+++ incl/nip.php
@@ -0,0 +1,65 @@
+<?php
+add_filter('woocommerce_checkout_fields', 'custom_woocommerce_checkout_fields', 999);
+
+function custom_woocommerce_checkout_fields($fields)
+{
+    $fields['billing']['billing_nip'] = array(
+        'type'        => 'text',
+        'label'       => __('NIP', 'woocommerce'),
+        'placeholder' => _x('Wpisz polski numer NIP', 'placeholder', 'woocommerce'),
+        'required'    => true,
+        'class'       => array('form-row-wide'),
+        'clear'       => true
+    );
+    return $fields;
+}
+/**
+ * Update the order meta with field value
+ */
+add_action('woocommerce_checkout_update_order_meta', 'custom_checkout_field_update_order_meta');
+
+function custom_checkout_field_update_order_meta($order_id)
+{
+    if (! empty($_POST['billing_nip'])) {
+        $order = wc_get_order($order_id);
+        $order->update_meta_data('NIP', sanitize_text_field($_POST['billing_nip']));
+        $order->save_meta_data();
+    }
+}
+/**
+ * Display field value on the order edit page
+ */
+add_action('woocommerce_admin_order_data_after_billing_address', 'custom_checkout_field_display_admin_order_meta', 10, 1);
+
+function custom_checkout_field_display_admin_order_meta($order)
+{
+    echo '<p><strong>' . esc_html__('NIP') . ':</strong> ' . esc_html($order->get_meta('NIP', true)) . '</p>';
+}
+/**
+ * Process the checkout
+ */
+add_action('woocommerce_checkout_process', 'custom_checkout_field_process');
+
+function custom_checkout_field_process()
+{
+    $valid = false;
+    $field_label = __('NIP', 'woocommerce');
+    $weights = array(6, 5, 7, 2, 3, 4, 5, 6, 7);
+    if (!empty($_POST['billing_nip'])) {
+        $value = sanitize_text_field($_POST['billing_nip']);
+        $nip = preg_replace('/^PL/', '', $value);
+        if (strlen($nip) == 10 && is_numeric($nip)) {
+            $sum = 0;
+
+            for ($i = 0; $i < 9; $i++)
+                $sum += $nip[$i] * $weights[$i];
+
+            $valid = ($sum % 11) == (int) $nip[9];
+        }
+        if ($valid === false) {
+            wc_add_notice(sprintf(__('Pole %s jest wypełnione niepoprawnie. Wpisz polski numer NIP bez spacji i myślników.', 'woocommerce'), '<strong>' . $field_label . '</strong>'), 'error', array(
+                'id' => 'billing_nip'
+            ));
+        }
+    }
+}
